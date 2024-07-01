aac1e96a37ab9ad65d99c8d959b689b258974bff
movingfrog <s24047@gsm.hs.kr>
2024-07-01 21:07:48 +0900

    moving and datafile

--- IdeaFestival/Assets/Scripts/PlayerMoving.cs
+++ IdeaFestival/Assets/Scripts/PlayerMoving.cs
@@ -1,18 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+
+    private Vector3 vector;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRun;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+
+    IEnumerator MoveCoroutine()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            applyRun = true;
+        }
+        else
+            applyRunSpeed = 0;
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while (currentWalkCount < walkCount)
+        {
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+            if(applyRun)
+                currentWalkCount++;
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0;
+        canMove = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if((Input.GetAxisRaw("Horizontal") != 0||Input.GetAxisRaw("Vertical") != 0)&&canMove)
+        {
+            canMove = false;
+            StartCoroutine(MoveCoroutine());
+        }  
     }
 }
