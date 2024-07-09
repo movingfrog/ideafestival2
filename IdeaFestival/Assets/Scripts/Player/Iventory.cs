96313e493225108c2b2f58cb0268d92e9ff7f895
movingfrog <s24047@gsm.hs.kr>
2024-07-09 11:39:53 +0900

    하는중2

--- IdeaFestival/Assets/Scripts/Player/Iventory.cs
+++ IdeaFestival/Assets/Scripts/Player/Iventory.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -8,17 +10,54 @@ public class ItemSlot
     public ItemData item;
     public int quatity;
 }
-public class Iventory : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ItemSlotUI[] uidSlot;
+    public ItemSlot[] slots;
+
+    public GameObject inventoryWindow;
+    public Transform dropPosition;
+
+    [Header("Selected Item")]
+    private ItemSlot selcteditem;
+    private int selcteditemindex;
+    public TextMeshPro selectedItemName;
+    public TextMeshPro selectedItemDescription;
+    public TextMeshPro selectedItemStatName;
+    public TextMeshPro selectedItemStatValue;
+    public GameObject useButton;
+    public GameObject dropButton;
+
+    public static Inventory instance;
+
+    bool IsOnInventory = false;
+
+    private void Awake()
     {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        inventoryWindow.SetActive(false);
+        slots = new ItemSlot[uidSlot.Length];
+
+        for (int i = 0; i < uidSlot.Length; i++)
+        {
+            slots[i] = new ItemSlot();
+            uidSlot[i].index = i;
+            uidSlot[i].Clear();
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddItem(ItemData item)
     {
-        
+        if (item.canStack)
+        {
+            //쌓일 수 있는 아이템일 경우 스택을 쌓는다
+            ItemSlot slotToStackTo;//= GetItemStack(item);
+        }
     }
 }
