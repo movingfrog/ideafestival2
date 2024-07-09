0ae8146b4effe1a2b3961a544048c2a2b19c4b26
movingfrog <s24047@gsm.hs.kr>
2024-07-09 11:19:00 +0900

    하는중

--- IdeaFestival/Assets/Scripts/Player/Iventory.cs
+++ IdeaFestival/Assets/Scripts/Player/Iventory.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class ItemSlot
+{
+    public ItemData item;
+    public int quatity;
+}
 public class Iventory : MonoBehaviour
 {
     // Start is called before the first frame update
--- IdeaFestival/Assets/Scripts/UI/Inventory/ItemSlotUI.cs
+++ IdeaFestival/Assets/Scripts/UI/Inventory/ItemSlotUI.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class ItemSlotUI : MonoBehaviour
 {
     public Button button;
-    public Image image;
+    public Image icon;
     public TextMeshPro quatityText;
     private ItemSlotUI curSlot;
 
@@ -13,5 +13,11 @@ public class ItemSlotUI : MonoBehaviour
 
     //아이템 Slot 정보 전달
     public void Set(ItemSlot slot)
+    {
+        curSlot = slot;
+        icon.gameObject.SetActive(false);
+        icon.sprite = slot.item.icon;
+        quatityText.text = slot.quatity > 1 ? slot.quatity :
+    }
 
 }
