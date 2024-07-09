6798aab635a3550eab02dcc9c3112937f06ae3e4
movingfrog <s24047@gsm.hs.kr>
2024-07-09 09:42:40 +0900

    ing

---
+++ IdeaFestival/Assets/Scripts/UI/Inventory/ItemSlotUI.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlotUI : MonoBehaviour
+{
+    public Button button;
+    public Image image;
+    public TextMeshPro quatityText;
+    private ItemSlotUI curSlot;
+
+    public int index;
+
+    //아이템 Slot 정보 전달
+    public void Set(ItemSlot slot)
+
+}
