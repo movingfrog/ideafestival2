11b14a5597cc08d757edcdfade3bcb9116c390c7
movingfrog <s24047@gsm.hs.kr>
2024-07-10 16:16:44 +0900

    asd

---
+++ IdeaFestival/Assets/Scripts/Player/GetItem.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GetItem : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("d");
+    }
+}
