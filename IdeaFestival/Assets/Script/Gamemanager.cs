dff889344de57c3da12a90130d602b2cb9c53265
pgsshiho <s24071@gsm.hs.kr>
2024-07-10 09:28:57 +0900

    asdas

--- IdeaFestival/Assets/Script/Gamemanager.cs
+++ IdeaFestival/Assets/Script/Gamemanager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gamemanager : MonoBehaviour
 {
@@ -10,12 +11,29 @@ public class Gamemanager : MonoBehaviour
     public GameObject BM;
     public GameObject Re;
     public GameObject QUit;
+    Mainmenu Mainmenu;
+    public Slider Volume;
+    public AudioSource audioSource;
 
     void Start()
     {
         // 패널과 어두운 배경을 처음에 비활성화
         panel.SetActive(false);
         darkBackground.SetActive(false);
+
+        // PlayerPrefs에서 슬라이더 값을 불러와 설정
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("Volume");
+            Volume.value = savedVolume; // 슬라이더 초기값을 저장된 값으로 설정
+            audioSource.volume = savedVolume; // 오디오 볼륨도 설정
+        }
+        else
+        {
+            Volume.value = audioSource.volume; // 슬라이더 초기값을 현재 오디오 볼륨으로 설정
+        }
+
+        Volume.onValueChanged.AddListener(SetVolume); // 슬라이더 값 변경 시 SetVolume 호출
     }
 
     void Update()
@@ -38,21 +56,30 @@ public class Gamemanager : MonoBehaviour
             }
         }
     }
+
     public void Backmenu()
     {
-        //메인메뉴로 가기
+        // 메인메뉴로 가기
         SceneManager.LoadScene("Mainmenu");
     }
+
     public void quit()
     {
-        //게임끄기
+        // 게임끄기
         Application.Quit();
     }
+
     public void REturn()
     {
-        //게임으로 돌아가기
+        // 게임으로 돌아가기
         Time.timeScale = 1;
         panel.SetActive(false);
         darkBackground.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume; // 오디오 볼륨을 슬라이더 값으로 설정
+        PlayerPrefs.SetFloat("Volume", volume); // 슬라이더 값을 PlayerPrefs에 저장
     }
 }
